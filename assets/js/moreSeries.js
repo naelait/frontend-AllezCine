d1e64d2211267bd6d1d888198d29cdfcc5847836
Nael <nael.ait@gmail.com>
2018-01-25 10:24:26 +0100

    rewrite cleaner code for more films/series.js

--- assets/js/moreSeries.js
+++ assets/js/moreSeries.js
@@ -1,98 +1,18 @@
-var plus = `
-<div class="row">
-  <div class="films-filtre">
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-  </div>
-</div>
-<div class="row">
-  <div class="films-filtre">
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-  </div>
-</div>
-`
-
-function MoreSeries() {
-  document.getElementById("series").innerHTML = ""
-  document.getElementById("series").innerHTML += plus
-  document.getElementById("series").innerHTML += plus
-  document.getElementById("series").innerHTML += `<div class="row">
-        <div class="col-md-12 d-flex justify-content-center">
-          <button onclick="LessSeries()" class="btn-sm nav-btn more" type="button">
-                Moins de series
-            </button>
-        </div>
-      </div>`
-}
-
-function LessSeries() {
-  document.getElementById("series").innerHTML = "";
-  document.getElementById("series").innerHTML += plus
-  document.getElementById("series").innerHTML += `<div class="row">
-        <div class="col-md-12 d-flex justify-content-center">
-          <button onclick="MoreSeries()" class="btn-sm nav-btn more" type="button">
-                Plus de series
-            </button>
-        </div>
-      </div>`;
+$('#plus-de-series').click(function() {
+  for (i of $('.toggle-series')) {
+    if (i.style.display == "") {
+      i.style.display = "block"
+    } else {
+      i.style.display = "none"
       $('html, body').animate({
-            scrollTop: $("#series").offset().top
-        }, 200);
-}
+        scrollTop: $("#series").offset().top
+      }, 200);
+    }
+  }
+  if (this.value == "Plus de series") {
+    this.value = "Moins de series"
+  } else {
+    this.value = "Plus de series"
+  }
+
+})
