8d7684fd9eb3f8e580dd1bdf36180547b166eb7f
jldenbroeder <jldenbroeder@gmail.com>
2018-01-25 13:26:21 +0100

    a merger

--- assets/js/moreFilms.js
+++ assets/js/moreFilms.js
@@ -1,98 +1,18 @@
-var plus = `
-<div class="row">
-  <div class="films-filtre">
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-  </div>
-</div>
-<div class="row">
-  <div class="films-filtre">
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-    <figure class="films-fig">
-      <img src="assets/img/slide1.jpg" alt="" class="films-slide">
-      <figcaption>titre</figcaption>
-      <figcaption><span class="left">date</span><span class="right">genre</span></figcaption>
-    </figure>
-  </div>
-</div>
-`
-
-function MoreMovies() {
-  document.getElementById("films").innerHTML = ""
-  document.getElementById("films").innerHTML += plus
-  document.getElementById("films").innerHTML += plus
-  document.getElementById("films").innerHTML += `<div class="row">
-        <div class="col-md-12 d-flex justify-content-center">
-          <button onclick="LessMovies()" class="btn-sm nav-btn more" type="button">
-                Moins de films
-            </button>
-        </div>
-      </div>`
-}
-
-function LessMovies() {
-  document.getElementById("films").innerHTML = "";
-  document.getElementById("films").innerHTML += plus
-  document.getElementById("films").innerHTML += `<div class="row">
-        <div class="col-md-12 d-flex justify-content-center">
-          <button onclick="MoreMovies()" class="btn-sm nav-btn more" type="button">
-                Plus de films
-            </button>
-        </div>
-      </div>`;
+$('#plus-de-films').click(function() {
+  for (i of $('.toggle-films')) {
+    if (i.style.display == "") {
+      i.style.display = "block"
+    } else {
+      i.style.display = "none"
       $('html, body').animate({
-            scrollTop: $("#films").offset().top
-        }, 200);
-}
+        scrollTop: $("#films").offset().top
+      }, 200);
+    }
+  }
+  if (this.value == "Plus de films") {
+    this.value = "Moins de films"
+  } else {
+    this.value = "Plus de Films"
+  }
+
+})
