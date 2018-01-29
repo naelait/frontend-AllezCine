0444510b2ba7a26970579b8e0cad65f0b72bb8b7
Nael <nael.ait@gmail.com>
2018-01-29 16:53:30 +0100

    adding JL work (footer random images + social side bar)

---
+++ assets/js/footerImg.js
@@ -0,0 +1,10 @@
+var myArray = [];
+for (i = 0; i < 6; i++) {
+  var nbrAleatoire = Math.floor(Math.random() * (15 - 1 + 1)) + 1;
+  if (myArray.includes(nbrAleatoire)) {
+    i--;
+  } else {
+    myArray.push(nbrAleatoire);
+    document.write('<img class="footAllezImg" src="assets/img/aff-xs' + nbrAleatoire + '.jpg" >');
+  }
+}
