630ff422b90c39cc82082c8352e5ef7eaed18438
Nael <nael.ait@gmail.com>
2018-01-30 12:28:02 +0100

    shop reworked, footer debug images

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,68 +1,98 @@
 var aff = [
-  ["Wonder Woman", "assets/img/aff-sm1.jpg", "2017", "Action", "https://www.youtube.com/embed/5lGoQhFb4NM","C'était avant qu'elle ne devienne Wonder Woman, à l'époque où elle était encore Diana, princesse des Amazones et combattante invincible. Un jour, un pilote américain s'écrase sur l'île paradisiaque où elle vit, à l'abri des fracas du monde."],
-  ["Mr Gaga", "assets/img/aff-sm2.jpg", "2015", "Comedy", "https://www.youtube.com/embed/F6gd8xpFMsM", "L’histoire fascinante d'Ohad Naharin, célèbre chorégraphe de la Batsheva Dance Company, dont les performances dégagent une puissance et une beauté inégalées. Le film nous dévoile le processus créatif d'un chef de file incontesté de la danse contemporaine, l’invention d’un langage chorégraphique unique et d’une technique de danse hors-norme appelée 'Gaga'."],
-  ["In The Fade", "assets/img/aff-sm3.jpg", "2014", "Adventure", "https://www.youtube.com/embed/UyjnzhXJlHU", "La vie de Katja est bouleversée lorsque son fils et son mari trouvent la mort dans un attentat. Katja est mariée au Kurde Nuri Sekerci, avec qui elle a un fils de cinq ans. Ils vivent à Hambourg. ... Au cours du procès, un expert scientifique décrit tous les détails macabres de l'attentat."],
-  ["X-Men", "assets/img/aff-sm4.jpg", "2015", "Fiction", "https://www.youtube.com/embed/tCbvZhVdPw0", "X-Men. Le Professeur Charles Xavier : C'est un mutant disposant de pouvoirs psychiques et télépathiques. Il a fondé l'institut qui porte son nom afin d'apprendre aux mutants à contrôler leurs pouvoirs. Pour lui, mutants et non-mutants peuvent coexister pacifiquement."],
-  ["Paddington 2", "assets/img/aff-sm5.jpg", "2013", "Comedy", "https://www.youtube.com/embed/qFuzMlfZGWM", "Là-bas, le petit ours embarque clandestinement à bord d'un cargo qui l'emmène à Londres. Sa tante reste au Pérou et s'installe dans une maison de retraite pour les ours où elle sera en sécurité. Puis l'ourson arrive à la gare de Paddington et est recueilli par la famille Brown (en théorie pour une seule nuit)."],
-  ["Safari", "assets/img/aff-sm6.jpg", "2016", "Comedy", "https://www.youtube.com/embed/ad8gbn0aMBs", "Six touristes français ont décidé de découvrir l'Afrique du Sud au cours d'un safari organisé par Richard Dacier. Ils ignorent alors que leur guide n'a pas mis les pieds dans la brousse depuis 30 ans... Et qu'il a peur des animaux !"],
-  ["Matrix", "assets/img/aff-sm33.jpg", "2007", "Fiction", "https://www.youtube.com/embed/m8e-FF8MsqU", "Toc Toc Neo. » Matrix (en France) ou La Matrice (au Québec et au Nouveau-Brunswick) (The Matrix) est un film australo-américain de science-fiction, un « cyberfilm», réalisé par Andy et Larry Wachowski et sorti en 1999. ... Il est considéré comme l'un des films du genre le plus important de son époque."],
-  ["Iron Man 2", "assets/img/aff-sm34.jpg", "2012", "Fiction", "https://www.youtube.com/embed/wKtcmiifycU", "Robert Downey Jr. dans le rôle de Tony Stark / Iron Man. Gwyneth Paltrow dans le rôle de Pepper Potts. Don Cheadle dans le rôle de lieutenant-colonel James « Rhodey » Rhodes / War Machine."]
+  ["Wonder Woman", "assets/img/aff-sm1.jpg", "2017", "Action", "https://www.youtube.com/embed/5lGoQhFb4NM", "C'était avant qu'elle ne devienne Wonder Woman, à l'époque où elle était encore Diana, princesse des Amazones et combattante invincible. Un jour, un pilote américain s'écrase sur l'île paradisiaque où elle vit, à l'abri des fracas du monde.", "0"],
+  ["Mr Gaga", "assets/img/aff-sm2.jpg", "2015", "Comedy", "https://www.youtube.com/embed/F6gd8xpFMsM", "L’histoire fascinante d'Ohad Naharin, célèbre chorégraphe de la Batsheva Dance Company, dont les performances dégagent une puissance et une beauté inégalées. Le film nous dévoile le processus créatif d'un chef de file incontesté de la danse contemporaine, l’invention d’un langage chorégraphique unique et d’une technique de danse hors-norme appelée 'Gaga'.", "1"],
+  ["In The Fade", "assets/img/aff-sm3.jpg", "2014", "Adventure", "https://www.youtube.com/embed/UyjnzhXJlHU", "La vie de Katja est bouleversée lorsque son fils et son mari trouvent la mort dans un attentat. Katja est mariée au Kurde Nuri Sekerci, avec qui elle a un fils de cinq ans. Ils vivent à Hambourg. ... Au cours du procès, un expert scientifique décrit tous les détails macabres de l'attentat.", "2"],
+  ["X-Men", "assets/img/aff-sm4.jpg", "2015", "Fiction", "https://www.youtube.com/embed/tCbvZhVdPw0", "X-Men. Le Professeur Charles Xavier : C'est un mutant disposant de pouvoirs psychiques et télépathiques. Il a fondé l'institut qui porte son nom afin d'apprendre aux mutants à contrôler leurs pouvoirs. Pour lui, mutants et non-mutants peuvent coexister pacifiquement.", "3"],
+  ["Paddington 2", "assets/img/aff-sm5.jpg", "2013", "Comedy", "https://www.youtube.com/embed/qFuzMlfZGWM", "Là-bas, le petit ours embarque clandestinement à bord d'un cargo qui l'emmène à Londres. Sa tante reste au Pérou et s'installe dans une maison de retraite pour les ours où elle sera en sécurité. Puis l'ourson arrive à la gare de Paddington et est recueilli par la famille Brown (en théorie pour une seule nuit).", "4"],
+  ["Safari", "assets/img/aff-sm6.jpg", "2016", "Comedy", "https://www.youtube.com/embed/ad8gbn0aMBs", "Six touristes français ont décidé de découvrir l'Afrique du Sud au cours d'un safari organisé par Richard Dacier. Ils ignorent alors que leur guide n'a pas mis les pieds dans la brousse depuis 30 ans... Et qu'il a peur des animaux !", "5"],
+  ["Matrix", "assets/img/aff-sm33.jpg", "2007", "Fiction", "https://www.youtube.com/embed/m8e-FF8MsqU", "Toc Toc Neo. » Matrix (en France) ou La Matrice (au Québec et au Nouveau-Brunswick) (The Matrix) est un film australo-américain de science-fiction, un « cyberfilm», réalisé par Andy et Larry Wachowski et sorti en 1999. ... Il est considéré comme l'un des films du genre le plus important de son époque.", "6"],
+  ["Iron Man 2", "assets/img/aff-sm34.jpg", "2012", "Fiction", "https://www.youtube.com/embed/wKtcmiifycU", "Robert Downey Jr. dans le rôle de Tony Stark / Iron Man. Gwyneth Paltrow dans le rôle de Pepper Potts. Don Cheadle dans le rôle de lieutenant-colonel James « Rhodey » Rhodes / War Machine.", "7"],
+  ["Pirates Of Caraïbes", "assets/img/aff-sm8.jpg", "2012", "Action", "https://www.youtube.com/embed/OTEGxxMTnPU", "Pirates des Caraïbes : La Malédiction du Black Pearl ou Pirates des Caraïbes : La Malédiction de la Perle noire, au Québec et au Nouveau-Brunswick (Pirates of the Caribbean: The Curse of the Black Pearl) est un film d'aventures américain de Gore Verbinski, sorti en 2003 et inspiré de l'attraction Pirates des Caraïbes de Walt Disney Parks and Resorts.", "8"],
+  ["Star Wars", "assets/img/aff-sm9.jpg", "2017", "Fiction", "https://www.youtube.com/embed/zB4I68XVPzQ", "L'univers de Star Wars est amorcé avec le conflit entre la République galactique et la Confédération des systèmes indépendants, alliance de planètes séparatistes. Cette confédération entraîne des dirigeants de la République dans des affaires ingérables à cause des manigances du Chancelier Suprême Palpatine, chef d'État de la République, qui dit servir la démocratie tout en visant secrètement à établir un empire. Pour rétablir la paix, les Jedi combattent les ennemis de la République, sans savoir que les deux camps sont manipulés par les Seigneurs Sith (Palpatine étant en réalité le Seigneur Sith Dark Sidious). Les Jedi, comme les Sith, sont sensibles à la Force, une capacité d'origine physiologique qui permet à leurs détenteurs d'acquérir certains pouvoirs surnaturels", "9"],
+  ["Avatar", "assets/img/aff-sm10.jpg", "2014", "Fiction", "https://www.youtube.com/embed/5PSNL1qE6VY", "Le personnage central de l'histoire est Jake Sully, un Marine paraplégique qui, via son avatar, va devoir choisir son camp avec pour enjeu, le destin de la planète. ... Il devient, après seulement six semaines d'exploitation, le plus gros succès de l'histoire du cinéma, battant Titanic, également réalisé par James Cameron.", "10"],
+  ["King Kong", "assets/img/aff-sm11.jpg", "2012", "Fiction", "https://www.youtube.com/embed/YAbI4w95cTE", "Kong: Skull Island est un film d'aventure américain réalisé par Jordan Vogt-Roberts, sorti en 2017. Il s'agit d'une nouvelle apparition du personnage de King Kong, créé pour le film du même nom sorti en 1933.", "11"],
+  ["La Croisée.", "assets/img/aff-sm12.jpg", "2014", "Adventure", "https://www.youtube.com/embed/KfwK41dUpuI", "Lyra, 12 ans, est une orpheline rebelle qui vit à Jordan College, un établissement de l'Université d'Oxford, dans un monde parallèle qui ressemble au nôtre mais qui a évolué de façon un peu différente. Elle a pour compagnon Pantalaimon, son dæmon, un être capable de prendre de nombreuses formes animales.", "12"],
+  ["Tintin", "assets/img/aff-sm13.jpg", "2013", "Adventure", "https://www.youtube.com/embed/9ua_4ajpP58", "Les Aventures de Tintin : Le Secret de La Licorne est un film d'aventure américano-néo-zélandais en capture de mouvement 3D réalisé par Steven Spielberg et produit par Peter Jackson, sorti en 2011", "13"],
+  ["Titanic", "assets/img/aff-sm14.jpg", "2006", "Adventure", "https://www.youtube.com/embed/2e-eXJ6HgkQ", "Titanic est une romance dramatique américaine écrite, produite et réalisée par James Cameron, sortie en 1997. Il raconte l'histoire de deux jeunes passagers du paquebot Titanic en avril 1912.", "14"],
+  ["Les Ch’Tis", "assets/img/aff-sm15.jpg", "2009", "Comedy", "https://www.youtube.com/embed/_6Mx3HZ4_tI", "Bienvenue chez les Ch'tis est un film français réalisé par Dany Boon, sorti le 20 février 2008 dans le Nord-Pas-de-Calais et dans quelques salles de la Somme, le 27 février 2008 dans le reste de la ...", "15"]
 ];
+
 var writeshop = ""
-var y = -1
-var x = 0
+var rnd = Math.floor(Math.random() * 16) + 0
+var temp = [];
 
 function createshopRow() {
+  temp.push(rnd)
+  writeshop = `${writeshop}<div class="col-3">
+    <div class="text-center">
+      <img id="${aff[rnd][6]}" class="shop-img img-fluid shop-item" src="${aff[rnd][1]}">
+    </div>
+    <div class="caption text-center">
+      ${aff[rnd][0]}</br>
+      ${aff[rnd][2]}, <span class="red">15€</span>
+    </div>
+  </div>`;
   document.getElementById("shop-js").innerHTML = writeshop
 }
 
-for (var i = 0; i < aff.length; i++) {
-  writeshop = `${writeshop}<div class="col-3">
-    <div class="text-center">
-      <img id="${i}" class="shop-img img-fluid shop-item" src="${aff[i][1]}">
-    </div>
-    <div class="caption text-center">
-      ${aff[i][0]}</br>
-      ${aff[i][2]}, <span class="red">15€</span>
-    </div>
-  </div>`;
-  y++
-  if (y < 8) {
+
+for (var i = 0; i < 8; i++) {
+  if (temp.includes(rnd) == true) {
+    rnd = Math.floor(Math.random() * 8) + 0
+    i--
+  } else {
     createshopRow()
   }
 }
-function showInfos(){
-  $('#title-js').html(`Titre: <span class="white">${aff[x][0]}</span>`)
-  $('#story-js').html(`Story line: <span class="white">${aff[x][5]}</span>`)
-  $('#release-js').html(`Année de Sortie: <span class="white">${aff[x][2]}</span>`)
-  $('#genre-js').html(`Genre: <span class="white">${aff[x][3]}</span>`)
-  $('#prix-js').html(`Prix: <span class="white">15€</span>`)
-  $('iframe').attr('src', aff[x][4])
-}
-showInfos()
-$("#next-js").click(function(){
-  x++
-  if (x>7){
-    x = 0
+
+
+
+$('#next-js').click(function() {
+  console.log("ok")
+  temp = []
+  i = 0
+  writeshop = ""
+  document.getElementById("shop-js").innerHTML = ""
+  for (var i = 0; i < 8; i++) {
+    if (temp.includes(rnd) == true) {
+      rnd = Math.floor(Math.random() * 16) + 0
+      i--
+    } else {
+      createshopRow()
+    }
   }
   showInfos()
 })
-$("#previous-js").click(function(){
-  x--
-  if (x<0){
-    x = 7
+$('#previous-js').click(function() {
+  console.log("ok")
+  temp = []
+  i = 0
+  writeshop = ""
+  document.getElementById("shop-js").innerHTML = ""
+  for (var i = 0; i < 8; i++) {
+    if (temp.includes(rnd) == true) {
+      rnd = Math.floor(Math.random() * 16) + 0
+      i--
+    } else {
+      createshopRow()
+    }
   }
   showInfos()
 })
 
-$('.shop-item').click(function(e){
-  e = e || window.event;
-  e = e.target || e.srcElement;
-  id = e.id
-  $('#title-js').html(`Titre: <span class="white">${aff[id][0]}</span>`)
-  $('#story-js').html(`Story line: <span class="white">${aff[id][5]}</span>`)
-  $('#release-js').html(`Année de Sortie: <span class="white">${aff[id][2]}</span>`)
-  $('#genre-js').html(`Genre: <span class="white">${aff[id][3]}</span>`)
-  $('#prix-js').html(`Prix: <span class="white">15€</span>`)
-  $('iframe').attr('src', aff[id][4])
-})
+function showInfos() {
+  $('.shop-item').click(function(e) {
+    e = e || window.event;
+    e = e.target || e.srcElement;
+    id = e.id
+    console.log(id)
+    $('#title-js').html(`Titre: <span class="white">${aff[id][0]}</span>`)
+    $('#story-js').html(`Story line: <span class="white">${aff[id][5]}</span>`)
+    $('#release-js').html(`Année de Sortie: <span class="white">${aff[id][2]}</span>`)
+    $('#genre-js').html(`Genre: <span class="white">${aff[id][3]}</span>`)
+    $('#prix-js').html(`Prix: <span class="white">15€</span>`)
+    $('iframe').attr('src', aff[id][4])
+  })
+}
+
+showInfos()
