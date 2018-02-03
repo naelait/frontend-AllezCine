fc8ec6446e747d5f89a2f2dfd846f5def5b27329
naelait <nael.ait@gmail.com>
2018-02-03 16:53:07 +0100

    Formulaire de contact

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,10 +1,20 @@
-function getContact()
-{
-// var hu = document.getElementById("hu").innerHTML;
-var fname = document.getElementById("contactFirstname").value;
-var lname = document.getElementById("contactLastname").value;
-var email = document.getElementById("contactEmail").value;
-var subjet = document.getElementById("contactSubject").value;
-var message = document.getElementById("contactMessage").value;
-alert(fname + "\n" + lname + "\n" + email + "\n" + subjet + "\n" + message);
-}
+$("#sendMessage-js").click(function() {
+  var firstName = $("#firstName-js").val()
+  var lastName = $("#lastName-js").val()
+  var email = $("#email-js").val()
+  var subject = $("#subject-js").val()
+  var message = $("#message-js").val()
+  alert(
+      `
+      First Name : ${firstName}
+
+      Last Name : ${lastName}
+
+      Email : ${email}
+
+      Subject : ${subject}
+
+      Message :
+      ${message}
+     `)
+})
